710849dab1a4f1476457aa96ac604f287b77773b
d5ng <dong5ffice@gmail.com>
2023-07-06 00:03:51 +0900

    Todo Remove

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import NewTodo from "./components/NewTodo"
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([])
-  const addTodoHandler = (todoText: string) =>
-    setTodos((prevTodo) => prevTodo.concat(new Todo(todoText)))
+  const addTodoHandler = (todoText: string) => setTodos((prevTodo) => prevTodo.concat(new Todo(todoText)))
+  const removeTodoHandler = (id: string) => setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id))
   return (
     <div>
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
     </div>
   )
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,15 +1,17 @@
 import React from "react"
 import Todo from "../models/Todo"
 import TodoItem from "./TodoItem"
+import classes from "./Todos.module.css"
 
 const Todos: React.FunctionComponent<{
   items: Todo[]
+  onRemoveTodo: (id: string) => void
 }> = (props) => {
   return (
     <React.Fragment>
-      <ul>
+      <ul className={classes.todos}>
         {props.items.map((item) => (
-          <TodoItem key={item.id} text={item.text} />
+          <TodoItem key={item.id} text={item.text} onRemoveTodo={props.onRemoveTodo.bind(null, item.id)} />
         ))}
       </ul>
     </React.Fragment>
